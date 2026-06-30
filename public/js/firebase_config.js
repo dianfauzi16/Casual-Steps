2dfd78e74cd3fd960dbdaa14df9aeea0348c95b0
M Dian Fauzi <dianf7673@gmail.com>
2026-07-01 00:54:26 +0700

    fix: un-ignore firebase_config.js and revert login image

---
+++ public/js/firebase_config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCo6EPyWMvNjRyXwX_gAMaBlaqPCIZNKcU",
+  authDomain: "casual-steps-118.firebaseapp.com",
+  projectId: "casual-steps-118",
+  storageBucket: "casual-steps-118.firebasestorage.app",
+  messagingSenderId: "63299401748",
+  appId: "1:63299401748:web:5b8dd5d2e8973ced3e4f01",
+  measurementId: "G-5RW66J5HNY"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
